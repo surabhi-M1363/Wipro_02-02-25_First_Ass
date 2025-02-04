8195e63a01017c0480b6ac5c578850aa97f75443
Surabhi_Reddy <surabhireddy148.com>
2025-02-04 09:48:14 +0530

    thirdquestions

---
+++ Question3.java
@@ -0,0 +1,46 @@
+// Base class
+class Item {
+    String title;
+    String author;
+    int publicationYear;
+
+    public Item(String title, String author, int publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+
+    public void displayDetails() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Publication Year: " + publicationYear);
+    }
+}
+
+// Derived class: Book
+class Book extends Item {
+    String genre;
+
+    public Book(String title, String author, int publicationYear, String genre) {
+        super(title, author, publicationYear);
+        this.genre = genre;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Genre: " + genre);
+    }
+}
+
+// Derived class: Magazine
+class Magazine extends Item {
+    int issueNumber;
+
+    public Magazine(String title, String author, int publicationYear, int issueNumber) {
+        super(title, author, publicationYear);
+        this.issueNumber = issueNumber;
+    }
+
+    @Override
+    public void display
